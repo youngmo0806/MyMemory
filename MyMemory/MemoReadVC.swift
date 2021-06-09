3de8e291967b590a7bf438cf83cc3540d1150a4d
youngmo <youngmo>
2021-06-09 15:58:24 +0900

    2021/06/09:First

---
+++ MyMemory/MemoReadVC.swift
@@ -0,0 +1,29 @@
+//
+//  MemoReadVC.swift
+//  MyMemory
+//
+//  Created by youngmo on 2021/06/09.
+//
+
+import UIKit
+
+class MemoReadVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
